841b17a53234374f89ffaf05dc30a528d3b1601c
swamyguradasu <swamyguradasu2527@gmail.com>
2026-09-25 14:07:11 +0530

    Configure vercel.json and resolve dependency resolution for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig} from 'vite';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': fileURLToPath(new URL('.', import.meta.url)),
       },
     },
     server: {
